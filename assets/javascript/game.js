a47ee2fe8a6e22a15f0974a5733c8e815e799402
Joanna Barajas <joannabarajas@joannabmacbook2.fios-router.home>
2017-02-21 15:41:13 -0800

    get friends to console.log random values, add jQuery script tag in HTML

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,106 @@
-$(document).ready(function() {
+//$(document).ready(function() {
 
-var winNumber = 0; // will hold target number to win, will be random
-// random (19,120)
+        var winNumber = Math.floor((Math.random() * 102) + 19); // will hold target number to win, will be random
+        // random (19,120)
 
-var hitNumber = 0; // will hold the value user hits
+        var hitsNumber = 0; // will hold the value user hits
 
-var crystalVal = 0; //random value for each crystal random(1, 12)
-
-var hitAdd = 0; //will hold the total value after successive hits
-
-var winCounter = 0;
-
-var lossCounter = 1;
-
-
-function startGame()
-/*
-	1. random value for game is selected and displayed to HTML
-	2. random values for crystals are assigned to their image
-	3. Starting score is zero, and our array or number list is always equal to zero
-*/
-
-function checkHit(hit) //function that gets input from user
-/*
-	1. Player hits crystal and hold that value
-	2. I want a conditional statement if the user value is less than "Win number" than user gets another try. 
-		If user value is over "Win number" then game ends
+        var totoVal = Math.floor((Math.random() * 12) + 1); //random value for each crystal random(1, 12)
  
-*/
+        var scarecrowVal = Math.floor((Math.random() * 12) + 1);
+        var tinmanVal = Math.floor((Math.random() * 12) + 1);
+        var lionVal = Math.floor((Math.random() * 12) + 1);
 
-function updateScore()
+
+        //var hitAdd = 0; //will hold the total value after successive hits
+
+        var wins = 0;
+
+        var losses = 1;
+
+
+
+
+        //1. random value for game is selected and displayed to HTML
+        //$(".targetNum").html(winNumber);
+
+
+        //1. Player hits crystal, onClick event and holds that value
+
+        $(".toto").on("click", function() {
+            update(totoVal);
+        });
+        $(".scarecrow").click(function() {
+            update(scarecrowVal);
+        });
+        $(".tinman").click(function() {
+            update(tinmanVal);
+        });
+        $(".lion").click(function() {
+            update(lionVal);
+        });
+        console.log(totoVal);
+        console.log(scarecrowVal);
+        console.log(tinmanVal);
+        console.log(lionVal);
+
+
+
+/*
+
+        // the startGame function is called when you win or lose a game.
+        function startGame() {
+            //generates new random number
+            winNumber = Math.floor((Math.random() * 102) + 19);
+            //displays number
+            $("#targetNum").html(winNumber);
+            //generates new values for friends
+            totoVal = Math.floor((Math.random() * 12) + 1);
+            scarecrowVal = Math.floor((Math.random() * 12) + 1);
+            tinmanVal = Math.floor((Math.random() * 12) + 1);
+            lionVal = Math.floor((Math.random() * 12) + 1);
+            // resets guesses to 0
+            hitsNumber = 0;
+            // display guess total
+            $("#realScore").html(hitsNumber);
+        
+        };
+
+
+
+        //This function will update the user's total guess value
+        function update(friendVal) {
+            //Increase the user's score according to chosen friend
+            hitsNumber += friendVal;
+            //removes old total and replaces it with new total
+            $("#realScore").html(empty);
+            $("#realScore").html(append);
+            //check to see if win or lose
+            if (hitsNumber > winNumber) {
+                //add a loss
+                losses++;
+                //displays loss
+                $("#loss-counter").html(losses);
+                alert("Oh no! Dorothy got caught up in another tornado!")
+                    // to reset game
+                startGame();
+            };
+        } else if(hitsNumber == targetNum) {
+            wins++;
+            $("#win-counter").html(wins);
+            alert("Great job, you got Dorothy and her friends to the Wizard!");
+            startGame();
+        });
+
+    
+
+});
+
+
+//function checkHit(hit) //function that gets input from user
+// 2. I want a conditional statement if the user value is less than "Win number" than user gets another try. 
+//	If user value is over "Win number" then game ends
+//function updateScore()
 /*
 	1. It's going to update the new score. Add the old value to the new value
 	2. It's going to update the HTML with the new value, and the "Win number"
