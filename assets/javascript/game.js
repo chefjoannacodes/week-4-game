f3843a9fac2606a1be9952025d46f16e7f01d149
Joanna Barajas <joannabarajas@joanna-barajass-macbook-2.local>
2017-02-24 13:46:30 -0800

    animate the images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
     var winNumber = Math.floor((Math.random() * 102) + 19); // will hold target number to win, will be random
     // random (19,120)
+    var newTotal = 0;
     var hasNumber = false;
 
     var totoVal = Math.floor((Math.random() * 12) + 1); //random value for each crystal random(1, 12)
@@ -28,7 +29,7 @@ console.log("this is the Win Number", winNumber);
    var tinmanVal = Math.floor((Math.random() * 12) + 1);
    var lionVal = Math.floor((Math.random() * 12) + 1);
    total = 0;
-   $("#realScore").html(total); // displays guess total to HTML
+   $("#realScore").html(newTotal); // displays guess total to HTML
 
 
 
@@ -46,8 +47,10 @@ function startGame() {
    var lionVal = Math.floor((Math.random() * 12) + 1);
    total = 0;
    $("#realScore").html(total); // displays guess total to HTML
+    
  };
 
+//function update() {
 
 // On click events for each character
 $("#toto").on("click", function() { //something happens when we click Toto
@@ -55,32 +58,101 @@ $("#toto").attr("value", totoVal);
   total += totoVal
   console.log("this is total toto", total);
 })
+  $("#toto").on("click", function() {
+      $("#toto").animate({ height:"210px", width: "210px"});
+      
+});
+$("#toto").on("click", function() {
+      $("#toto").animate({ height:"200px", width: "200px"});
+});
 
 $("#scarecrow").on("click", function() { //something happens when we click Scarecrow  
 $("#scarecrow").attr("value", scarecrowVal);
   total += scarecrowVal
   console.log("this is total scarecrow", total);
-})
+  })
+$("#scarecrow").on("click", function() {
+      $("#scarecrow").animate({ height:"210px", width: "210px"});
+});
+$("#scarecrow").on("click", function() {
+      $("#scarecrow").animate({ height:"200px", width: "200px"});
+});
+
 
 $("#tinman").on("click", function() { //something happens when we click Tinman
 $("#tinman").attr("value", tinmanVal);
   total += tinmanVal
   console.log("this is total tinman", total);
 })
+$("#tinman").on("click", function() {
+      $("#tinman").animate({ height:"210px", width: "210px"});
+});
+$("#tinman").on("click", function() {
+      $("#tinman").animate({ height:"200px", width: "200px"});
+});
 
 $("#lion").on("click", function() { //something happens when we click Lion
 $("#lion").attr("value", tinmanVal);
   total += tinmanVal
   console.log("this is total lion", total);
-
 })
+$("#lion").on("click", function() {
+      $("#lion").animate({ height:"210px", width: "210px"});
+});
+$("#lion").on("click", function() {
+      $("#lion").animate({ height:"200px", width: "200px"});
+});
 
-$("#realScore").html(total);
-$("#realScore").append(total); //*****This displays the updated Guess Total for the User on HTML
-console.log("i updated total", total);
-
+function update(character) {
+    total += totoVal + tinmanVal + scarecrowVal + lionVal;
+console.log("string total", total);
+ newTotal = parseInt(total);
+ console.log("integer total", newTotal);
+$("#realScore").html(newTotal);
+};
+//$("#realScore").append(total); //*****This displays the updated Guess Total for the User on HTML
+//console.log("i updated total", total);
 
 });
+
+function winOrLose() {
+    $("#realScore").empty();
+    $("#realScore").append(newTotal);
+    console.log("I'm the updated total", total);
+
+if (newTotal > winNumber) {
+    // adds a loss
+    losses++;
+    console.log("our loss", losses);
+    // display updated losses.
+    $("#loss-counter").html(losses);
+    //calls startGame to reset
+    startGame();
+} else if (newTotal == winNumber) {
+    //add a win
+    wins++;
+    //display updated win
+    $("#win-counter").html(wins);
+    //calls the startGame to reset
+    startGame();
+
+    };
+
+
+/**/
+};
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
